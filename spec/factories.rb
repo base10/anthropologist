db6a7cb28bfb04dd14f103543a49454db3b45bd7
Nathan L. Walls <nathan@wallscorp.us>
2017-04-18 22:56:42 -0400

    Add initial author model, migration

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,4 +1,9 @@
 FactoryGirl.define do
+  factory :author do
+    name "Jane Committer"
+    sequence(:email) { |n| "j.committer+#{n}@example.com" }
+  end
+
   factory :repository do
     sequence(:name) { |n| "fun_repo_name_#{n}" }
     url "https://github.com/example/fun_repo_name"
